8731b43dfc6d469df05d0bb065d4aca5c3729fa4
Sufiyan Masutikhani <masutikhanisufiyan@gmail.com>
2025-03-21 01:16:39 +0530

    change ALLOWED_HOSTS again with localhost

--- college_site/settings.py
+++ college_site/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG","False").lower() == "true"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost 127.0.0.1").split(" ")
+ALLOWED_HOSTS = ['college-website-m7gt.onrender.com','Localhost','127.0.0.1']
 
 
 # Application definition
