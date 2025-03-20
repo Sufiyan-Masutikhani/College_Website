21993b81101d035da24da9a17b33fb7f37e2e460
Sufiyan Masutikhani <masutikhanisufiyan@gmail.com>
2025-03-21 01:28:45 +0530

    Changed Settings.py by adding static_root and added static file

--- college_site/settings.py
+++ college_site/settings.py
@@ -22,6 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Add this line
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
 
 
 # Quick-start development settings - unsuitable for production
